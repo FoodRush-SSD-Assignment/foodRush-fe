52187b4e9ebe9a6c41306ed04fc5a0c08c177d58
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-04-26 11:03:39 +0530

    connect add to cart section with backend

--- src/components/restaurant/ItemCard.jsx
+++ src/components/restaurant/ItemCard.jsx
@@ -1,10 +1,26 @@
 // src/components/restaurant/MenuItemCard.jsx
 import React from 'react';
+import orderApi from '../../api/orderApi';
 
 const ItemCard = ({ item }) => {
-  const handleAddToCart = () => {
-    console.log(`Added ${item.itemName} to cart`);
-  };
+  const handleAddToCart = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await orderApi.post("/order-service/cart/addToCart",
+        { itemIds: [item._id] },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Cart updated:", response.data);
+      alert(`Added ${item.itemName} to cart`);
+    } catch (error) {
+      console.error("Error adding to cart:", error.response?.data || error.message);
+      alert("Failed to add item to cart");
+    }
+  }
 
   return (
     <div className="bg-lightgray rounded-2xl border border-darkgrey shadow-sm w-full p-6 flex flex-col justify-between min-h-[200px]">
