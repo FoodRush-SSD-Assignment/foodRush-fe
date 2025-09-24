dedf9e0fac6e7bb8626a708b6882402ebe0e1a51
Sahan Kaviska <it22233530@my.sliit.lk>
2025-09-24 23:27:06 +0530

    parth travesel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), 
+    },
+  },
+  server: {
+    fs: {
+      strict: true,
+      allow: [
+        path.resolve(__dirname, "public"),
+        path.resolve(__dirname, "src"), //
+        path.resolve(__dirname, "node_modules")
+      ],
+    },
+  },
 });
